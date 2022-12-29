854ca41998ecf7eca97fcda3692c0b130fa3e4a3
Rakib099 <rakibnabi97@gmail.com>
2022-12-29 15:36:20 +0600

    Remaining slots set based on booking

---
+++ __api-naming.js
@@ -0,0 +1,10 @@
+/* API Naming Convention */
+
+/*
+app.get('/bookings')      => getting all the bookings
+app.get('/bookings/:id')  => getting a particular booking data
+app.post('/bookings')     => submitting a booking
+app.patch('/bookings/:id')  => updating a particular booking
+app.delete('/bookings/:id')`=> deleting a particular booking
+
+*/
--- index.js
+++ index.js
@@ -13,19 +13,54 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.i9w8jvi.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
 
+        const bookingCollection = client.db("doctorsPortal").collection("bookings");
+
+        // use aggregate to query multiple collection and merge data
         app.get('/appointmentOptions', async (req, res) => {
-            const query = {}
+            const date = req.query.date;
+            const query = {};
             const cursor = appointmentOptionCollection.find(query);
             const options = await cursor.toArray();
+            // get the booking of the provided day
+            const bookingQuery = {appointmentDate: date};
+            const alreadyBooked = await bookingCollection.find(bookingQuery).toArray();
+            options.forEach(option => {
+
+                // booking based on an option
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                // slots booked for an option
+                const bookedSlots = optionBooked.map(book => book.slot);
+                // remaining slots for an option
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+
+                // console.log(date, option.name, remainingSlots.length)
+                option.slots = remainingSlots;
+            })
             res.send(options);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                treatment: booking.treatment,
+                email: booking.email
+            }
+            const alreadyBooked = await bookingCollection.find(query).toArray();
+            if (alreadyBooked.length) {
+                const message = `You already have an appointment on ${booking.appointmentDate}`;
+                return res.send({acknowledged: false, message});
+            }
+
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
         });
     }
     finally {
