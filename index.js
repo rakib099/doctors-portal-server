2be00d29f10b8777cab340d8ba4a9903911e9864
Rakib099 <rakibnabi97@gmail.com>
2022-12-30 10:16:43 +0600

    Users get api added

--- index.js
+++ index.js
@@ -113,6 +113,13 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users  = await cursor.toArray();
+            res.send(users);
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
